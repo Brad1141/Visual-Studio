44ae2cee3958a501eb430a33ba7502536b735d5b
Brad LaMontagne <bradlamontagne1141@gmail.com>
2017-10-27 17:08:08 -0400

    Commit 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is version 2!");
+            string userinput = Console.ReadLine();
+            if (userinput == "Hello") {
+                Console.WriteLine("Hello to you too!");
+            }
+        }
+    }
+}
